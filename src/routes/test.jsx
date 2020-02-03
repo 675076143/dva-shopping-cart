a7f344ce100a6fa43b85a2212b52bbc771801095
675076143 <36965465+675076143@users.noreply.github.com>
2020-02-03 13:30:59 +0800

    first commit

---
+++ src/routes/test.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+import {reqProducts} from "../services/example";
+
+export  default  class Test extends Component{
+  componentDidMount() {
+    reqProducts().then(res=>{
+      console.log(res);
+    })
+  }
+
+  render() {
+    return(
+      <div className="test">
+        this is test
+      </div>
+    )
+  }
+}
